c492e97218aa0ac199bdba56566a5376042cc544
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2023-12-09 11:38:07 +0000

    Create armv7.sh

---
+++ .github/workflows/armv7.sh
@@ -0,0 +1,30 @@
+# Install dependencies
+sudo apt-get update
+sudo apt-get -y install clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev binutils coreutils desktop-file-utils fakeroot fuse libgdk-pixbuf2.0-dev patchelf python3-pip python3-setuptools squashfs-tools strace util-linux zsync file unzip zip
+
+# Install dart
+mkdir submodules/flutter/bin/cache
+export LAST_PWD=$PWD
+cd submodules/flutter/bin/cache
+wget https://storage.googleapis.com/dart-archive/channels/stable/release/3.1.0/sdk/dartsdk-linux-arm-release.zip
+unzip dartsdk-linux-arm-release.zip
+rm dartsdk-linux-arm-release.zip
+cd $LAST_PWD
+
+# Install flutter
+submodules/flutter/bin/flutter doctor
+export PATH="$PATH:$PWD/submodules/flutter/bin"
+
+# Configure flutter
+flutter config --no-analytics
+
+# Build with updates popup
+bash build_all_with_updates_popup.sh
+
+# Prepare releases
+cd ..
+mkdir -p build/linux-bundle/Passy
+cp -r Passy/build/linux/x64/release/bundle/. build/linux-bundle/Passy
+cp Passy/build/appimage/Passy-Latest-x86_64.AppImage build
+chmod +x build/linux-bundle/Passy/passy
+chmod +x build/Passy-Latest-x86_64.AppImage
