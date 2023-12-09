02812f920a03ce18d42a817a0fbb3048d6ccc673
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2023-12-09 11:57:04 +0000

    Update armv7.sh

--- .github/workflows/armv7.sh
+++ .github/workflows/armv7.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
 cd /Passy
 
-# Install dependencies
-apt-get update
-apt-get -y install clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev binutils coreutils desktop-file-utils fakeroot fuse libgdk-pixbuf2.0-dev patchelf python3-pip python3-setuptools squashfs-tools strace util-linux zsync file unzip zip wget
+echo "===================================================="
+echo "Install dependencies"
+echo "===================================================="
 
-# Install dart
+apt-get update
+apt-get -y install clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev binutils coreutils desktop-file-utils fakeroot fuse libgdk-pixbuf2.0-dev patchelf python3-pip python3-setuptools squashfs-tools strace util-linux zsync git file unzip zip wget
+
+echo "===================================================="
+echo "Install dart"
+echo "===================================================="
+
 mkdir submodules/flutter/bin/cache
 export LAST_PWD=$PWD
 cd submodules/flutter/bin/cache
@@ -14,17 +20,29 @@ unzip dartsdk-linux-arm-release.zip
 rm dartsdk-linux-arm-release.zip
 cd $LAST_PWD
 
-# Install flutter
+echo "===================================================="
+echo "Install flutter"
+echo "===================================================="
+
 submodules/flutter/bin/flutter doctor
 export PATH="$PATH:$PWD/submodules/flutter/bin"
 
-# Configure flutter
+echo "===================================================="
+echo "Configure flutter"
+echo "===================================================="
+
 flutter config --no-analytics
 
-# Build with updates popup
+echo "===================================================="
+echo "Build with updates popup"
+echo "===================================================="
+
 bash build_all_with_updates_popup.sh
 
-# Prepare releases
+echo "===================================================="
+echo "Prepare releases"
+echo "===================================================="
+
 cd /passy-build
 mkdir -p linux-bundle/Passy
 cp -r /Passy/build/linux/x64/release/bundle/. linux-bundle/Passy
